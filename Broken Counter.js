b17ef8ccf7ac962eca6084fc8e0251da4373d03d
Olga Lubsanova <lubsanova07@gmail.com>
2023-03-08 09:09:37 -0600

    Broken Counter

---
+++ Broken Counter.js
@@ -0,0 +1,20 @@
+// DESCRIPTION:
+// Our counter prototype is broken. Can you spot, what's wrong here?
+//
+// SOLUTION:
+
+function Counter() {
+    this.value = 0;
+};
+
+Counter.prototype.increase = function() {
+    this.value++;
+};
+
+Counter.prototype.getValue = function() {
+    return this.value;
+};
+
+Counter.prototype.reset = function() {
+    this.value = 0;
+};
