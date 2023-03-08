b182db422565ce978337ee48bbe1a767a2ed73c0
Olga Lubsanova <lubsanova07@gmail.com>
2023-03-08 09:01:00 -0600

    Regexp Basics - is it a digit?

---
+++ Regexp Basics - is it a digit.js
@@ -0,0 +1,9 @@
+// DESCRIPTION:
+// Implement String#digit? (in Java StringUtils.isDigit(String)), which should return true if given object
+// is a digit (0-9), false otherwise.
+//
+// SOLUTION:
+
+String.prototype.digit = function() {
+    return /^\d$/.test(this);
+}
